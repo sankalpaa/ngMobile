f46f90206ba4ed4729a7b5d1492f3650f54314e0
guththila2 <guththiladev02@gmail.com>
2015-10-15 18:07:28 +0530

    add ng-touch

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,13 @@
-angular.module('ngMobile', [])
+angular.module('ngMobile', ['ngTouch'])
     .controller('home', ['$scope', function (scope) {
         scope.title = "Home Page";
         scope.clickMe = function() {
             alert("Yes");
+        };
+        scope.swipeLeft = function() {
+            alert('left');
+        };
+        scope.swipeRight = function () {
+            alert('Right');
         };
     }]);
